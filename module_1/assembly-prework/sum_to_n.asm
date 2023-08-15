40e0c0fa7d0cc97b0ad325fc8d49eab6aaa39bc9
Emil Reji <emil315.reji@gmail.com>
2023-08-14 21:27:09 -0400

    minor chnage

--- module_1/assembly-prework/sum_to_n.asm
+++ module_1/assembly-prework/sum_to_n.asm
@@ -2,7 +2,7 @@ section   .text
 global    _start
 
 _start:
-    mov       rdi, n		; n value
+	mov		  rdi, n		; n value
 	xor 	  rax, rax		; setting rax to 0
 .loop:
 	add		  rax, rdi		; accumulates sum to rax
@@ -13,8 +13,8 @@ print:
 	mov 	  rsi, rax
 	mov       rdx, 8
 	syscall
-	mov       rax, 60       ; system call for exit
-    xor       rdi, rdi      ; exit code 0
-    syscall                 ; invoke operating system to exit
+	mov		  rax, 60       ; system call for exit
+	xor		  rdi, rdi      ; exit code 0
+	syscall                 ; invoke operating system to exit
 
 n  	equ       5
